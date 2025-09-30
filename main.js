dd26353120e8f5728c44d66ccbb7f16b9f48e005
Maxi-raph <onwujekwechibuzor@gmail.com>
2025-09-30 22:32:06 +0100

    update

--- main.js
+++ main.js
@@ -27,14 +27,28 @@ const hideCashes2 = document.querySelectorAll('.hide-cash2')
 const imgContainers2 = document.querySelectorAll('.img-container2')
 const heartIcons2 = document.querySelectorAll('.hearts2')
 const slideDots2 = document.querySelectorAll('.slider-dots2')
-const videoframe = document.querySelector('.videoframe')
 
+const productTrack3 = document.querySelector('.products-track3')
+const products3 = document.querySelectorAll('.products3')
+const addCarts3 = document.querySelectorAll('.add-cart3')
+const hideCashes3 = document.querySelectorAll('.hide-cash3')
+const imgContainers3 = document.querySelectorAll('.img-container3')
+const heartIcons3 = document.querySelectorAll('.hearts3')
+const slideDots4 = document.querySelectorAll('.slider-dots4')
+
+const videoframe = document.querySelector('.videoframe')
+const testimonialsContain = document.querySelector('.testimonials-container')
+const testimonialsTrack = document.querySelector('.testimonials-track')
+const testimonials = document.querySelectorAll('.testimonials')
+const slideDots3 = document.querySelectorAll('.slider-dots3')
 // ===============================
 // SLIDER HELPERS
 // ===============================
 let heroIndexRef = { value: 0 }
 let prodIndexRef = { value: 0 }
 let prodIndex2Ref = { value: 0 }
+let prodIndex3Ref = { value: 0 }
+let testIndexRef = { value: 0 }
 let startX = 0
 let endX = 0
 
@@ -50,10 +64,10 @@ function touchmove(event, width, track, indexRef) {
   track.style.transform = `translateX(${offset}px)`
 }
 
-function touchend(track, contents, width, dots = [], indexRef) {
+function touchend(track, contents, width, dots = [], indexRef, visibleCount) {
   delta = endX - startX
   if (event.target.classList.contains('hearts') || event.target.classList.contains('hearts2')) return
-  if (delta < -50 && indexRef.value < contents.length - 2) {
+  if (delta < -50 && indexRef.value < contents.length - visibleCount) {
     indexRef.value++
   } else if (delta > 50 && indexRef.value > 0) {
     indexRef.value--
@@ -179,6 +193,10 @@ let trackObserver = new IntersectionObserver((entries) => {
       heroIndexRef.value = 0
       prodIndexRef.value = 0
       prodIndex2Ref.value = 0
+      prodIndex3Ref.value = 0
+      testIndexRef.value = 0
+      heroTrack.style.transition = 'transform 0.6s ease'
+      heroTrack.style.transform = `translateX(0px)`
       productTrack.style.transition = 'transform 0.6s ease'
       productTrack.style.transform = `translateX(0px)`
       slideDots.forEach(dot => dot.classList.remove('bg-stone-400'))
@@ -187,8 +205,14 @@ let trackObserver = new IntersectionObserver((entries) => {
       productTrack2.style.transform = `translateX(0px)`
       slideDots2.forEach(dot => dot.classList.remove('bg-stone-400'))
       slideDots2[0].classList.add('bg-stone-400')
-      heroTrack.style.transition = 'transform 0.6s ease'
-      heroTrack.style.transform = `translateX(0px)`
+      testimonialsTrack.style.transition = 'transform 0.6s ease'
+      testimonialsTrack.style.transform = `translateX(0px)`
+      slideDots3.forEach(dot => dot.classList.remove('bg-stone-400'))
+      slideDots3[0].classList.add('bg-stone-400')
+      productTrack3.style.transition = 'transform 0.6s ease'
+      productTrack3.style.transform = `translateX(0px)`
+      slideDots4.forEach(dot => dot.classList.remove('bg-stone-400'))
+      slideDots4[0].classList.add('bg-stone-400')
     }
   })
 }, { threshold: 0 })
@@ -198,7 +222,7 @@ productsContainer.forEach(container => {
 })
 
 trackObserver.observe(heroContain)
-
+trackObserver.observe(testimonialsContain)
 
 // ===============================
 // PRODUCT CLICK HANDLERS
@@ -255,6 +279,32 @@ products2.forEach(product => {
   })
 })
 
+products3.forEach(product => {
+  product.addEventListener('click', (e) => {
+    e.stopPropagation()
+    let addCart = product.querySelector('.add-cart3')
+    let hideCash = product.querySelector('.hide-cash3')
+    let imgContainer = product.querySelector('.img-container3')
+    let heartIcon = product.querySelector('.hearts3')
+    
+    if (hideCash.classList.contains('translate-y-[-20px]')) {
+      if (e.target.classList.contains('hearts3')) {
+        e.target.classList.toggle('text-red-500')
+        e.target.classList.remove('hidden')
+        //I'll add the favorites in an array here so that once i click the heart icon at the top, i'll go to a site where all the favourites are in a row
+        return
+      }
+      window.location.href = ''
+    } else {
+      clickedProduct(e, addCarts3, hideCashes3, imgContainers3, heartIcons3)
+      hideCash.classList.add('translate-y-[-20px]')
+      addCart.classList.add('translate-y-[-23px]')
+      imgContainer.classList.add('scale-[0.9]')
+      heartIcon.classList.remove('hidden')
+    }
+  })
+})
+
 function clickedProduct(e, addCarts, hideCashes, imgContainers, heartIcons) {
   addCarts.forEach(cart => cart.classList.remove('translate-y-[-23px]'))
   hideCashes.forEach(cash => cash.classList.remove('translate-y-[-20px]'))
@@ -276,20 +326,24 @@ document.addEventListener('click', () => {
   hideCashes2.forEach(cash => cash.classList.remove('translate-y-[-20px]'))
   imgContainers2.forEach(contain => contain.classList.remove('scale-[0.9]'))
   heartIcons2.forEach(heart => heart.classList.add('hidden'))
+  
+  addCarts3.forEach(cart => cart.classList.remove('translate-y-[-23px]'))
+  hideCashes3.forEach(cash => cash.classList.remove('translate-y-[-20px]'))
+  imgContainers3.forEach(contain => contain.classList.remove('scale-[0.9]'))
+  heartIcons3.forEach(heart => heart.classList.add('hidden'))
 })
 
-
-// ===============================
-// PRODUCT SLIDER TOUCH
-// ===============================
 window.addEventListener('load', () => {
-    let productsStyle = window.getComputedStyle(productTrack)
+  // ===============================
+  // PRODUCT SLIDER TOUCH
+  // ===============================
+  let productsStyle = window.getComputedStyle(productTrack)
   let prodGap = parseInt(productsStyle.rowGap) || 0
   let prodPad = parseInt(productsStyle.paddingLeft) || 0
   let prodWidth = products[0].offsetWidth + prodGap
   productTrack.addEventListener('touchstart', (e) => { touchstart(e, productTrack) })
   productTrack.addEventListener('touchmove', (e) => { touchmove(e, prodWidth, productTrack, prodIndexRef) })
-  productTrack.addEventListener('touchend', () => { touchend(productTrack, products, prodWidth, slideDots, prodIndexRef) })
+  productTrack.addEventListener('touchend', () => { touchend(productTrack, products, prodWidth, slideDots, prodIndexRef, 2) })
   
   
   let productsStyle2 = window.getComputedStyle(productTrack2)
@@ -298,5 +352,34 @@ window.addEventListener('load', () => {
   let prodWidth2 = products2[0].offsetWidth + prodGap2
   productTrack2.addEventListener('touchstart', (e) => { touchstart(e, productTrack2) })
   productTrack2.addEventListener('touchmove', (e) => { touchmove(e, prodWidth2, productTrack2, prodIndex2Ref) })
-  productTrack2.addEventListener('touchend', () => { touchend(productTrack2, products2, prodWidth2, slideDots2, prodIndex2Ref) })
+  productTrack2.addEventListener('touchend', () => { touchend(productTrack2, products2, prodWidth2, slideDots2, prodIndex2Ref, 2) })
+  
+  let productsStyle3 = window.getComputedStyle(productTrack3)
+  let prodGap3 = parseInt(productsStyle3.rowGap) || 0
+  let prodPad3 = parseInt(productsStyle3.paddingLeft) || 0
+  let prodWidth3 = products3[0].offsetWidth + prodGap3
+  productTrack3.addEventListener('touchstart', (e) => { touchstart(e, productTrack3) })
+  productTrack3.addEventListener('touchmove', (e) => { touchmove(e, prodWidth3, productTrack3, prodIndex3Ref) })
+  productTrack3.addEventListener('touchend', () => { touchend(productTrack3, products3, prodWidth3, slideDots4, prodIndex3Ref, 2) })
+  
+  
+  // ===============================
+  // TESTIMONIALS SLIDER TOUCH
+  // ===============================
+  
+  let testStyles = window.getComputedStyle(testimonials[0])
+  let testWidth = parseInt(testStyles.width)
+  
+  testimonialsTrack.addEventListener('touchstart', (e) => {
+    touchstart(e, testimonialsTrack)
+  })
+  
+  testimonialsTrack.addEventListener('touchmove', (e) => {
+    touchmove(e, testWidth, testimonialsTrack, testIndexRef)
+  })
+  
+  testimonialsTrack.addEventListener('touchend', () => {
+    touchend(testimonialsTrack, testimonials, testWidth, slideDots3, testIndexRef, 1)
+  })
+  
 })
